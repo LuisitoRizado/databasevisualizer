0b8b764f1e55feb82753bec0bc1b2a3207048297
LuisitoRizado <laog.sonic4@gmail.com>
2024-12-08 18:46:06 -0600

    Tailwind second fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
-	darkMode: ["class"],
-	content: [
-	  "./src/**/*.{js,jsx,ts,tsx}",
-	],
-	theme: {
-	  extend: {
-		borderRadius: {
-		  lg: 'var(--radius)',
-		  md: 'calc(var(--radius) - 2px)',
-		  sm: 'calc(var(--radius) - 4px)'
-		},
-		colors: {}
-	  }
-	},
-	plugins: [require("tailwindcss-animate")],
-  };
-  
+  darkMode: ["class"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {}
+    }
+  },
+  plugins: [tailwindcssAnimate],
+};
